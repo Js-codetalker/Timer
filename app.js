e0f5fcbff69bf812b556fc663a88651d415d6e1d
CodeTalker <jcsa031403@gmail.com>
2021-07-13 01:30:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let hours = document.querySelector('.remainHours'),
+	minutes = document.querySelector('.remainMinutes'),
+	seconds = document.querySelector('.remainSeconds'),
+	starButton = document.querySelector('.startTimer');
+
+const countdown = element => {
+		element.value = ("0"+(parseInt(element.value)-1)).slice(-2)
+}
+starButton.addEventListener('click', ()=> {
+
+	let interval = setInterval(()=> {
+		if (seconds.value === "00" && parseInt(minutes.value) > 0) {
+			countdown(minutes)
+			seconds.value = "60"
+		}
+		if (seconds.value === "00" && parseInt(minutes.value) == 0 && parseInt(hours.value) > 0) {
+			countdown(hours)
+			minutes.value = "59"
+			seconds.value = "60"
+		} 
+		if (seconds.value === "00" && parseInt(minutes.value) == 0 && parseInt(hours.value) == 0) {
+			clearInterval(interval)
+		}
+		countdown(seconds)
+	},1000)
+})
