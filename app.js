52d27f48c92b36fb71a9c13be6dcafd25eb35e1a
CodeTalker <jcsa031403@gmail.com>
2021-07-16 08:22:42 -0500

    alarm sound added

--- app.js
+++ app.js
@@ -1,28 +1,66 @@
-let hours = document.querySelector('.remainHours'),
-	minutes = document.querySelector('.remainMinutes'),
-	seconds = document.querySelector('.remainSeconds'),
-	starButton = document.querySelector('.startTimer'),
-	title = document.getElementById('title');
+let hours = { element : document.querySelector('.remainHours'),
+			  configuredTime : document.querySelector('.remainHours').value,
+			  remainTime : document.querySelector('.remainHours').value },
+
+	minutes = { element : document.querySelector('.remainMinutes'),
+				configuredTime : document.querySelector('.remainMinutes').value,
+				remainTime :  document.querySelector('.remainMinutes').value },
+
+	seconds = { element : document.querySelector('.remainSeconds'),
+				configuredTime : document.querySelector('.remainSeconds').value,
+				remainTime : document.querySelector('.remainSeconds').value},
+
+	startButton = document.querySelector('.timer__startButton'),
+	resetButton = document.querySelector('.timer__resetButton'),
+	audio = document.querySelector('.alarm')
+	title = document.getElementById('title'),
+	interval = setInterval(()=> {},1000);
+
+	clearInterval(interval);
+
 
 const countdown = element => {
-		element.value = ("0"+(parseInt(element.value)-1)).slice(-2)
+		element.remainTime = ("0"+(parseInt(element.remainTime)-1)).slice(-2)
+		element.element.value = element.remainTime
 }
-starButton.addEventListener('click', ()=> {
+const resetTimer = () => {
+	resetButton.addEventListener('click',()=> {
+	clearInterval(interval);
+	hours.remainTime = hours.configuredTime;
+	minutes.remainTime = minutes.configuredTime;
+	seconds.remainTime = seconds.configuredTime;
+	hours.element.value = hours.configuredTime;
+	minutes.element.value = minutes.configuredTime;
+	seconds.element.value = seconds.configuredTime;
 
-	let interval = setInterval(()=> {
-		if (seconds.value === "00" && parseInt(minutes.value) > 0) {
+	starTimer();
+})
+}
+const starTimer = () => {
+	startButton.addEventListener('click', ()=> {
+		hours.remainTime = document.querySelector('.remainHours').value,
+		minutes.remainTime = document.querySelector('.remainMinutes').value
+		seconds.remainTime = document.querySelector('.remainSeconds').value
+
+	interval = setInterval(()=> {
+		if (seconds.remainTime === "00" && parseInt(minutes.remainTime) > 0) {
 			countdown(minutes)
-			seconds.value = "60"
+			seconds.remainTime = "60"
 		}
-		if (seconds.value === "00" && parseInt(minutes.value) == 0 && parseInt(hours.value) > 0) {
+		if (seconds.remainTime === "00" && parseInt(minutes.remainTime) == 0 && parseInt(hours.remainTime) > 0) {
 			countdown(hours)
-			minutes.value = "59"
-			seconds.value = "60"
+			minutes.remainTime = "59"
+			seconds.remainTime = "60"
 		} 
-		if (seconds.value === "00" && parseInt(minutes.value) == 0 && parseInt(hours.value) == 0) {
-			clearInterval(interval)
+		if (seconds.remainTime === "00" && parseInt(minutes.remainTime) == 0 && parseInt(hours.remainTime) == 0) {
+			audio.play()
+			resetTimer()
 		}
 		countdown(seconds)
-		title.innerHTML = `${hours.value}:${minutes.value}:${seconds.value}`
+		title.innerHTML = `${hours.remainTime}:${minutes.remainTime}:${seconds.remainTime}`
 	},1000)
 })
+}
+
+starTimer()
+
